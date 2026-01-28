6c07ac746f0d6fcd125d5fb056ea090ffc17afeb
MageSantana <33928559+MageSantana@users.noreply.github.com>
2026-01-28 18:56:39 +0100

    Update Package.swift
    Signed-off-by: MageSantana <33928559+MageSantana@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LyraMaterial",
-            url: "https://github.com/MageSantana/Material-test-spm/releases/download/1.0.8/LyraMaterial.xcframework.zip",
+            url: "https://raw.githubusercontent.com/MageSantana/Material-test-spm/1.0.8/LyraMaterial.xcframework.zip",
             checksum: "8e96770fd7377348d869fc5c25c7745ce1470994848b52f13a70a2d32504add9"
         )
     ]
