01d97a7d9969584d070984f0baeae2e9f5cab750
Magela Santana <magela.santana@lyra-network.com>
2026-01-29 16:49:04 +0100

    fix package with dependecy

--- Package.swift
+++ Package.swift
@@ -21,8 +21,15 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LyraMaterial",
-            url: "https://raw.githubusercontent.com/MageSantana/Material-test-spm/1.0.10/LyraMaterial.xcframework.zip",
+            url: "https://raw.githubusercontent.com/MageSantana/Material-test-spm/1.0.11/LyraMaterial.xcframework.zip",
             checksum: "8e96770fd7377348d869fc5c25c7745ce1470994848b52f13a70a2d32504add9"
+        ),
+        .target(
+            name: "LyraMaterialWrapper",
+            dependencies: [
+                "LyraMaterial",
+                .product(name: "LyraMotion", package: "LyraMotion")
+            ]
         )
     ]
 )
