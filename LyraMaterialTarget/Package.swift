0a7b521f74a90035b3aacfce5d67cd7d8423ae93
Magela Santana <magela.santana@lyra-network.com>
2026-01-30 10:36:04 +0100

    test dependecy

---
+++ LyraMaterialTarget/Package.swift
@@ -0,0 +1,7 @@
+// Ce fichier est nécessaire pour que SPM reconnaisse le target wrapper
+// Il permet de lier le binary target avec ses dépendances
+
+import Foundation
+
+@_exported import LyraMaterialBinary
+@_exported import LyraMotion
